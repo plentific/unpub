5c65f8826dc1ef8682ac908a05b8ed271a80e156
Jakub Jodełka <jakub@jodelka.me>
2023-02-21 13:52:28 +0100

    Add s3 storing for test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dart ./unpub/bin/unpub.dart -d ${DB_URL} --proxy-origin ${HOST_NAME} -e true
+dart ./unpub/bin/unpub.dart -d ${DB_URL} --proxy-origin ${HOST_NAME} -e true -s3bucket 'bucket' -s3accessKey 'access' -s3secretKey 'secret' -s3region 'region'
--- unpub/bin/unpub.dart
+++ unpub/bin/unpub.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
-import 'package:path/path.dart' as path;
+
 import 'package:args/args.dart';
 import 'package:mongo_dart/mongo_dart.dart';
+import 'package:path/path.dart' as path;
 import 'package:unpub/unpub.dart' as unpub;
+import 'package:unpub_aws/unpub_aws.dart';
 
 main(List<String> args) async {
   var parser = ArgParser();
@@ -11,6 +13,10 @@ main(List<String> args) async {
   parser.addOption('database', abbr: 'd', defaultsTo: 'mongodb://localhost:27017/dart_pub');
   parser.addOption('proxy-origin', abbr: 'o', defaultsTo: '');
   parser.addOption('exitOnDbError', abbr: 'e', defaultsTo: 'false');
+  parser.addOption('s3bucket', abbr: 'b', defaultsTo: '');
+  parser.addOption('s3accessKey', abbr: 'a', defaultsTo: '');
+  parser.addOption('s3secretKey', abbr: 's', defaultsTo: '');
+  parser.addOption('s3region', abbr: 'r', defaultsTo: '');
 
   var results = parser.parse(args);
 
@@ -19,6 +25,10 @@ main(List<String> args) async {
   var dbUri = results['database'] as String;
   var proxy_origin = results['proxy-origin'] as String;
   var exitOnDbError = (results['exitOnDbError'] as String?) == 'true';
+  var s3Bucket = results['s3bucket'] as String;
+  var s3access = results['s3accessKey'] as String;
+  var s3secret = results['s3secretKey'] as String;
+  var s3region = results['s3region'] as String;
 
   if (results.rest.isNotEmpty) {
     print('Got unexpected arguments: "${results.rest.join(' ')}".\n\nUsage:\n');
@@ -29,11 +39,13 @@ main(List<String> args) async {
   final db = Db(dbUri);
   await db.open();
 
-  var baseDir = path.absolute('unpub-packages');
-
   var app = unpub.App(
       metaStore: unpub.MongoStore(db, onDatabaseError: exitOnDbError ? () => exit(1) : null),
-      packageStore: unpub.FileStore(baseDir),
+      packageStore: S3Store(
+        s3Bucket,
+        region: s3region,
+        credentials: AwsCredentials(awsAccessKeyId: s3access, awsSecretAccessKey: s3secret),
+      ),
       proxy_origin: proxy_origin.trim().isEmpty ? null : Uri.parse(proxy_origin));
 
   var server = await app.serve(host, port);
