2105d48ba3153c3c6a886ab56a673e5a7568b599
Dawid Bladzich <dawid.bladzich@plentific.com>
2023-02-24 11:14:07 +0100

    Changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dart ./unpub/bin/unpub.dart -d ${DB_URL} --proxy-origin ${HOST_NAME} -e true
+dart ./unpub/bin/unpub.dart -d ${DB_URL} --proxy-origin ${HOST_NAME} -e true --roleArn ${AWS_ROLE_ARN} --roleSessionName 'unpubS3Store' --webIdentityTokenFile ${AWS_WEB_IDENTITY_TOKEN} --bucketName ${AWS_BUCKET_NAME}
